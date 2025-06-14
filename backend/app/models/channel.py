a5aa15e8333f6d56a058862996a67a9f9adbae24
Cramessar <154475437+Cramessar@users.noreply.github.com>
2025-06-13 21:50:42 -0400

    fixed channel.py formatting

--- backend/app/models/channel.py
+++ backend/app/models/channel.py
@@ -7,12 +7,12 @@ from app.models.base import Base
 class Channel(Base):
     __tablename__ = "channel"
 
-id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-id_text: Mapped[Optional[str]] = mapped_column(String(15), unique=True)
-slug: Mapped[Optional[str]] = mapped_column(String(100), unique=True)
-feed_id: Mapped[Optional[int]] = mapped_column(Integer, unique=True)
-podcast_index_id: Mapped[Optional[int]] = mapped_column(Integer)
-podcast_guid: Mapped[Optional[str]] = mapped_column(UUID(as_uuid=False), unique=True)
-title: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
-sortable_title: Mapped[Optional[str]] = mapped_column(String(255), unique=True)
-medium_id: Mapped[Optional[int]] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id_text: Mapped[Optional[str]] = mapped_column(String(15), unique=True)
+    slug: Mapped[Optional[str]] = mapped_column(String(100), unique=True)
+    feed_id: Mapped[Optional[int]] = mapped_column(Integer, unique=True)
+    podcast_index_id: Mapped[Optional[int]] = mapped_column(Integer)
+    podcast_guid: Mapped[Optional[str]] = mapped_column(UUID(as_uuid=False), unique=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    sortable_title: Mapped[Optional[str]] = mapped_column(String(255), unique=True)
+    medium_id: Mapped[Optional[int]] = mapped_column(Integer)
