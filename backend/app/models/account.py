a39c93a26b9cc076b39f8b7553f2ff5cf932ff5e
Garrett Cross <garrettcrosskc@gmail.com>
2025-06-16 13:19:17 -0500

    added StatsTrackAccountGuid modal

--- backend/app/models/account.py
+++ backend/app/models/account.py
@@ -15,3 +15,11 @@ class SharableStatus(Base):
     
     id: Mapped[int] = mapped_column(primary_key=True)
     status: Mapped[str] = mapped_column(String(50))
+    
+class StatsTrackAccountGuid(Base):
+    __tablename__ = "stats_track_account_guid"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    account_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("account.id"), nullable=False)
+    account_guid: Mapped[Optional[str]] = mapped_column(String(36), nullable=True)
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, server_default=db.func.now(), onupdate=db.func.now())
