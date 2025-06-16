9815c36774c8592f06f4174ee85bf32d9ab7a20a
Garrett Cross <garrettcrosskc@gmail.com>
2025-06-16 11:27:38 -0500

    added stats_aggregated_channel model

--- backend/app/models/channel.py
+++ backend/app/models/channel.py
@@ -16,3 +16,26 @@ podcast_guid: Mapped[Optional[str]] = mapped_column(UUID(as_uuid=False), unique=
 title: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
 sortable_title: Mapped[Optional[str]] = mapped_column(String(255), unique=True)
 medium_id: Mapped[Optional[int]] = mapped_column(Integer)
+
+class StatsAggreatedChannel(Base):
+    __tablename__ = "stats_aggregated_channel"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    channel_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("channel.id"), nullable=False)
+    day_current_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_1_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_2_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_3_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_4_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_5_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_6_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_7_count: Mapped[int] = mapped_column(Integer, default=0)
+    day_8_count: Mapped[int] = mapped_column(Integer, default=0)
+    week_current_count: Mapped[int] = mapped_column(Integer, default=0)
+    week_1_count: Mapped[int] = mapped_column(Integer, default=0)
+    week_2_count: Mapped[int] = mapped_column(Integer, default=0)
+    week_3_count: Mapped[int] = mapped_column(Integer, default=0)
+    week_4_count: Mapped[int] = mapped_column(Integer, default=0)
+    month_current_count: Mapped[int] = mapped_column(Integer, default=0)
+    month_1_count: Mapped[int] = mapped_column(Integer, default=0)
+    all_time_count: Mapped[int] = mapped_column(Integer, default=0)
