2bf0d41c8300fbed5de3dd20e5bedc402e72700c
Garrett Cross <garrettcrosskc@gmail.com>
2025-06-16 11:20:31 -0500

    added feed_log and feed_flag_status models

--- backend/app/models/feed.py
+++ backend/app/models/feed.py
@@ -15,3 +15,21 @@ class Feed(Base):
     updated_at: Mapped[DateTime] = mapped_column(DateTime, server_default=db.func.now(), onupdate=db.func.now())
     # set to time when parsing starts and NULL when parsing ends
     is_parsing: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    
+class FeedFlagStatus(Base):
+    __tablename__ = "feed_flag_status"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=db.func.now())
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, server_default=db.func.now(), onupdate=db.func.now())
+    status: Mapped[str] = mapped_column(String(50))
+    
+class FeedLog(Base):
+    __tablename__ = "feed_log"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    feed_id: Mapped[int] = mapped_column(db.ForeignKey("feed.id"))
+    last_http_status: Mapped[Optional[int]] = mapped_column(db.Integer)
+    last_good_http_status_time: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    last_finished_parse_time: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    parse_errors: Mapped[Optional[int]] = mapped_column(db.Integer, default=0)
