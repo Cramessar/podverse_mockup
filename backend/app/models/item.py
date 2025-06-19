3a73df15b71598d30529d21ff098695094886c60
Garrett Cross <garrettcrosskc@gmail.com>
2025-06-18 18:49:39 -0600

    update Item model

--- backend/app/models/item.py
+++ backend/app/models/item.py
@@ -10,12 +10,12 @@ class Item(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     id_text: Mapped[Optional[str]] = mapped_column(String(15), unique=True)
     channel_id: Mapped[int] = mapped_column(db.ForeignKey("channel.id"), nullable=False)
-    #item_flag_status_id: Mapped[Optional[int]] = mapped_column(db.ForeignKey("item_flag_status.id"), nullable=True)
+    item_flag_status_id: Mapped[Optional[int]] = mapped_column(db.ForeignKey("item_flag_status.id"), nullable=True)
     title: Mapped[str] = mapped_column(String(300))
     slug: Mapped[str] = mapped_column(String(300), unique=True)
     guid:  Mapped[str] = mapped_column(String(300), unique=True)
     guid_enclosure_url:  Mapped[Optional[str]] = mapped_column(String(500), unique=True)
-    published_at: Mapped[Optional[DateTime]] = mapped_column(DateTime)
+    pub_date: Mapped[Optional[DateTime]] = mapped_column(DateTime)
     
 class ItemFlagStatus(Base):
     __tablename__ = "item_flag_status"
