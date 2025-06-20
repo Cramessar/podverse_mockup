00d94c6cfc5452d4b331255dfa63c190943330bf
Noel Watters <noelwatters757@gmail.com>
2025-06-20 12:45:37 -0400

    fixed merge conflict

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -15,8 +15,25 @@ export async function middleware(request) {
   const session = await auth0.getSession();
   if (!session) {
     const { origin } = new URL(request.url);
+<<<<<<< Updated upstream
     return NextResponse.redirect(`${origin}/auth/login`);
   }
 
   return authRes;
+=======
+    return NextResponse.redirect(`${origin}/auth/login?returnTo=/dashboard`);
+  }
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         * - api (API routes)
+         */
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)",
+    ],
+>>>>>>> Stashed changes
 }
