2af0caa59aa426a6c8fbfa2deab86bc80a6b862c
Garrett Cross <garrettcrosskc@gmail.com>
2025-06-21 22:23:31 -0500

    added ValidationError

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from marshmallow import fields, validate
+from marshmallow import fields, validate, ValidationError
 # from flask_migrate import Migrate 
 
 db = SQLAlchemy()
