f2838f3f599fa11f3b81e2ce3e406effe83cbed3
Garrett Cross <garrettcrosskc@gmail.com>
2025-06-21 21:55:05 -0500

    moved ma fields and validate to extensions.py

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import fields, validate
 # from flask_migrate import Migrate 
 
 db = SQLAlchemy()
